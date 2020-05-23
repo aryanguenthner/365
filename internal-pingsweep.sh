67e9ffab05251e4e950c14afa69c8049ce8f1840
Aryan <aryanguenthner@users.noreply.github.com>
2020-05-22 18:09:39 -0700

    Add files via upload

--- internal-pingsweep.sh
+++ internal-pingsweep.sh
@@ -2,19 +2,21 @@
 
 ##################################################
 # Discover Live Hosts by doing a ping sweep
+# Run Nmap Scan
 # Live Hosts Are Output To A File targets.txt
 # Learn More at https://github.com/aryanguenthner/
-# Tested on Kali 2020-05-07
+# Tested on Kali 2020.2
 ##################################################
 
 YELLOW='033m'
 DATE=date
 KALI=`hostname -I`
-SUBNET='$KALI\24'
+SUBNET=subnet.txt
 TARGETS=targets.txt
 
 echo "Ping Sweep Date"
 echo -e "\e[033m`date`\e[0m"
+echo
 echo -e "\e[033mOutput Directory /home/kali/Desktop/nmap-collector/\e[0m"
 echo
 mkdir -p /home/kali/Desktop/nmap-collector/
@@ -26,7 +28,7 @@ echo
 echo -e "\e[033mThe Target Subnet\e[0m"
 ip route | awk 'NR==2' | awk '{print $1}' 2>&1 | tee subnet.txt
 echo
-echo -e "\e[033mGenerating a Target List\e[0m"
+echo -e "\e[033mLaunching Ping Sweep and Generating a Target List\e[0m"
 nmap -iL $SUBNET -sn -n --exclude $KALI -oG nmap
 echo
 echo -e "\e[033mThese Hosts Are Up\e[0m"
@@ -37,6 +39,10 @@ echo -e "\e[033mTarget File Created targets.txt\e[0m"
 echo
 echo -e "\e[033mUse nmap to get more info on your targets\e[0m"
 echo
-echo "nmap -iL targets.txt -mtu 24 -T4 -Pn -sS -A -r -p- --open --max-retries 0 -vvvv -oA nmapscan"
+echo "Launching Scan"
 echo
-mv subnet.txt targets.txt nmap* /home/kali/Desktop/nmap-collector/
+nmap -iL targets.txt -mtu 24 -T4 -Pn -sS -A -r -p- --open --max-retries 0 -vvvv -oA /home/kali/Desktop/nmap-collector/nmapscan
+echo
+echo
+mv subnet.txt targets.txt nmap /home/kali/Desktop/nmap-collector/
+echo
