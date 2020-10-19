ec76e8d1cf5634b1185d3c50d83ece6c84d64c57
Aryan <aryanguenthner@users.noreply.github.com>
2020-10-18 17:21:52 -0700

    Add files via upload

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #######################################################
 # Discover Live Hosts by Doing a Ping Sweep
-# Live Hosts Are Output To A File targets.txt
+# Live Hosts Are Output To A File ips.txt
 # Learn More at https://github.com/aryanguenthner/
-# Tested on Kali 2020.1
+# Tested on Kali 2020.3
 ######################################################
 
 YELLOW='033m'
 EXCLUDE=excludefile.txt
-SUBNET=subnet.txt
+SUBNET=`ip r |awk 'NR==2' |awk '{print $1}'`
 TARGETS=targets.txt
 #KALI=`ifconfig eth0 | grep "inet" | awk '{print $2}' | awk 'NR==1'` > excludefile.txt
 KALI=`hostname -I`
@@ -21,17 +21,16 @@ echo -e "\e[033mKali IP\e[0m"
 hostname -I
 echo
 echo -e "\e[033mThe Target Subnet\e[0m"
-nmap --iflist | awk 'NR==9' | awk '{print $1}' 2>&1 | tee subnet.txt
-echo
+echo $SUBNET
 echo -e "\e[033mGenerating a Target List\e[0m"
-nmap -sn -n -iL $SUBNET --exclude $KALI -oG nmap
+nmap -sn -n $SUBNET --exclude $KALI -oG nmap
 echo
 echo -e "\e[033mThese Hosts Are Up\e[0m"
 echo
-cat nmap | grep "Up" | awk '{print $2}' 2>&1 | tee targets.txt
+cat nmap | grep "Up" | awk '{print $2}' 2>&1 | tee ips.txt
 echo
-echo -e "\e[033mTarget File Created targets.txt\e[0m"
+echo -e "\e[033mTarget List Ouput File -> ips.txt\e[0m"
 echo
-echo -e "\e[033mUse nmap to get more info on your targets\e[0m"
+echo -e "\e[033mUse nmap to enumerate more info on your targets\e[0m"
 echo
-echo "nmap -iL targets.txt -sT -A -p- -r --open --max-retries 0 -oA nmapscan"
+echo "Try: nmap -iL ips.txt -vvvv -sT -A -p- -r --open --max-retries 0 -mtu 24 -oA nmapscan"
