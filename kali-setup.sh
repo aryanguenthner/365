ecbbbc509bcd4983f52df29c94ffd65b351c917c
Aryan <aryanguenthner@users.noreply.github.com>
2020-11-23 09:56:10 -0800

    Add files via upload

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 ################################################
 # Kali Post Setup Automation Script
-# Tested on Kali 2020.3
+# Tested on Kali 2020.4
 # If you're reading this pat yourself on the back
-# sudo dos2unix kali-setup.sh
-# sudo chmod +x kali-setup.sh 
-# Usage type: sudo ./kali-setup.sh | tee setuplog.txt
+# sudo dos2unix *.sh
+# sudo chmod +x *.sh
+# Usage: sudo ./kali-setup.sh | tee setup.log
 # Learn more at https://github.com/aryanguenthner/
-# Last Updated 2020-10-16
+# Last Updated 11/20/2020
 ################################################
-#python-pygraphviz python-psycopg2 python-krbv python-mysqldb phantomjs<-- //Might need these one day
+#python-dbus python-pygraphviz python-lxml python-pil python-crypto python-psycopg2 python-krbv python-mysqldb <-- //Might need these one day
+date | tee kali-setup-startdate.txt
 echo
 echo "Be Patient, Installing Kali Dependencies"
-sudo apt update && apt -y upgrade && apt -y install crackmapexec python-crypto hostapd dnsmasq gedit cupp nautilus dsniff build-essential cifs-utils cmake curl ffmpeg gimp git graphviz imagemagick libapache2-mod-php php-xml libappindicator3-1 libindicator3-7 libmbim-utils libreoffice nfs-common openssl python3-dev python-dbus python-lxml python-pil terminator tesseract-ocr vlc wkhtmltopdf xsltproc xutils-dev python3-venv driftnet websploit apt-transport-https openresolv screenfetch baobab speedtest-cli sendmail
+sudo apt update && apt -y upgrade && apt -y install crackmapexec hostapd dnsmasq gedit cupp nautilus dsniff build-essential cifs-utils cmake curl ffmpeg gimp git graphviz imagemagick libapache2-mod-php php-xml libappindicator3-1 libindicator3-7 libmbim-utils nfs-common openssl terminator tesseract-ocr vlc wkhtmltopdf xsltproc xutils-dev driftnet websploit apt-transport-https openresolv screenfetch baobab speedtest-cli sendmail python3-dev python3-venv python3-pip libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev awscli sublist3r libreoffice
+echo
 cd /home/kali/Desktop
 echo
 echo "VPN stuff"
@@ -20,7 +22,6 @@ cd /tmp
 wget --no-check-certificate https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
 apt-key add openvpn-repo-pkg-key.pub
 echo
-date 2>&1 | tee kali-setup-startdate.txt
 echo
 # Because We like SSH
 echo "Enabling SSH"
@@ -28,40 +29,53 @@ sudo sed -i '32s/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/s
 sudo systemctl enable ssh
 sudo service ssh restart
 echo
+echo "Teleconsole Rocks"
+curl https://www.teleconsole.com/get.sh | sh
+# Run: teleconsole -f localhost:5000
+echo
 echo "Your Internal IP Address"
 hostname -I
 echo
-echo '# Kali IP' >> /root/.bashrc
+echo "External Internal IP Address"
+curl ifconfig.me
+echo
+echo '# IP Address' >> /root/.bashrc
 echo 'hostname -I' >> /root/.bashrc
 echo
 echo '#Go' >> /root/.bashrc
 echo 'export GOPATH=$HOME/work' >> /root/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> /root/.bashrc
 echo
+# Metasploit
 cd /opt
 sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
 echo "Metasploit Ready Up"
 sudo systemctl start postgresql
 sudo msfdb init
 echo
-echo "IVRE Rocks"
-sudo apt-get -y install ivre
-echo
 echo "Getting Vulsec & Vulners to make Nmap Do cool things"
 cd /usr/share/nmap/scripts/
-git clone https://github.com/vulnersCom/nmap-vulners.git
-git clone https://github.com/scipag/vulscan.git
+sudo git clone https://github.com/vulnersCom/nmap-vulners.git
+sudo git clone https://github.com/scipag/vulscan.git
 cd vulscan/utilities/updater/
-chmod +x updateFiles.sh
-./updateFiles.sh
+sudo chmod +x updateFiles.sh
+sudo ./updateFiles.sh
 echo
+echo "Enabling Nmap Screenshots Getting PhantomJS"
+cd /opt
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+tar xvf phantomjs-1.9.8-linux-x86_64.tar.bz2
+mv phantomjs-1.9.8-linux-x86_64 phantomjs
+mv phantomjs /opt
+ln -s /opt/phantomjs/bin/phantomjs /usr/local/bin/phantomjs
+phantomjs -v
 # Yeet
 echo "Fixing setoolkit"
 # Fix Social Engineer Toolkit
 # Remove old files
 sudo rm -R /usr/share/set
 # Get the lastest files
-git clone https://github.com/trustedsec/social-engineer-toolkit.git
+sudo git clone https://github.com/trustedsec/social-engineer-toolkit.git
 # Rename
 sudo mv social-engineer-toolkit set
 # Move the new files back to the correct path
@@ -71,37 +85,34 @@ echo "Cloud Tool Time"
 # Get Pip
 # Cloudsecurity Tool Time
 # https://rhinosecuritylabs.com/aws/pacu-open-source-aws-exploitation-framework/
-echo "Pacu The Fish, Not the Rapper"
-cd /opt
-git clone https://github.com/RhinoSecurityLabs/pacu
-cd pacu
-sudo bash install.sh
-sudo pip3 install -r requirements.txt
-echo
+#echo "Pacu The Fish, Not the Rapper"
+#cd /opt
+#git clone https://github.com/RhinoSecurityLabs/pacu
+#cd pacu
+#sudo bash install.sh
+#sudo pip3 install -r requirements.txt
+#echo
 # Usage: python3 pacu.py
 #Scout
-echo "AWS Scout"
-cd /opt 
-git clone https://github.com/nccgroup/ScoutSuite.git
-# Usage scout aws --profile basc -f
-echo
-echo "Wayback"
-cd /opt
-git clone git clone https://github.com/Rhynorater/waybacktool.git
-echo
-cd /opt
-git clone https://github.com/GerbenJavado/LinkFinder.git
-cd LinkFinder
-sudo pip3 install -r requirements.txt
-sudo python setup.py install
-echo
+#echo "AWS Scout"
+#cd /opt 
+#git clone https://github.com/nccgroup/ScoutSuite.git
+## Usage scout aws --profile basc -f
+#echo
+#echo "Wayback"
+#cd /opt
+#git clone git clone https://github.com/Rhynorater/waybacktool.git
+#echo
+#cd /opt
+##git clone https://github.com/GerbenJavado/LinkFinder.git
+#cd LinkFinder
+#sudo pip3 install -r requirements.txt
+#sudo python setup.py install
+#echo
 cd /opt
 echo "ShellPhish"
 cd /opt
 git clone https://github.com/aryanguenthner/shellphish.git
-echo
-echo "AWS CLI"
-sudo pip install awscli
 echo
 echo "Malicious Macro Builder"
 cd /opt
@@ -122,18 +133,11 @@ cd /opt
 git clone https://github.com/TheRook/subbrute.git
 echo
 echo
-cd /opt
-git clone https://github.com/aboul3la/Sublist3r.git
-cd Sublist3r
-sudo pip install -r requirements.txt
-sudo apt-get -y install python-requests
-sudo apt-get -y install python-dnspython
-echo
 echo "dnstwister"
 cd /opt
 git clone https://github.com/elceef/dnstwist.git
 sudo apt-get -y install python3-dnspython python3-geoip python3-whois \
-python3-requests python3-ssdeep
+python3-requests python3-ssdeep python3-dns
 echo
 echo "RDPY"
 cd /opt
@@ -180,6 +184,10 @@ echo "Payloads All The Things"
 cd /opt
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 echo
+echo "OneListForAll"
+cd /opt
+git clone https://github.com/six2dez/OneListForAll.git
+7z x onelistforall.7z.001
 echo "Awesome XSS"
 cd /opt
 git clone https://github.com/s0md3v/AwesomeXSS.git
@@ -243,11 +251,10 @@ unzip gitrob_linux_amd64_2.0.0-beta.zip
 mkdir -p /opt/gitrob
 mv gitrob /opt/gitrob/
 echo
-echo "Google Play CLI"
-apt -y install gplaycli
+#echo "Google Play CLI"
+#apt -y install gplaycli
 echo
 echo
-sudo apt install -y python3-venv python3-pip python3-dev build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
 # MobSF Setup
 echo "MobSF"
 cd /opt/
@@ -259,26 +266,39 @@ echo
 echo "Lee Baird Discover Script"
 cd /opt
 git clone https://github.com/leebaird/discover.git
-echo "Just Don't Update Using the Discover Update Script"
-echo "Don't Blink"
+echo "Just Don't Update Kali Using the Lee Baird Discover Update Script"
 echo
 # Save these two for later
 # git clone https://github.com/jschicht/RawCopy.git
 # git clone https://github.com/khr0x40sh/MacroShop.git
 echo
-#echo "My Cool Scripts"
-#cd /opt
-#git clone https://github.com/aryanguenthner/365.git
-#cd 365
-#dos2unix *.sh *.py && chmod +x *.sh *.py
-#echo
-#Tor
+echo "My Cool Scripts"
+cd /opt
+git clone https://github.com/aryanguenthner/365.git
+cd 365
+dos2unix *.sh *.py && chmod +x *.sh *.py
+echo
+echo
+cd /opt
+wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
+tar xf Python-3.6.5.tar.xz
+cd Python-3.6.5
+./configure
+make
+sudo make install
+echo
+# Update Python Alternatives Lists
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+update-alternatives --install /usr/bin/python python /usr/local/bin/python3.6 2
+update-alternatives --install /usr/bin/python python /usr/bin/python3.8 3
+# Tor
 sudo gpg --keyserver pool.sks-keyservers.net --recv-keys EB774491D9FF06E2 && apt -y install torbrowser-launcher
 sudo chmod -R 777 /home/kali/
 echo
 echo "Hacker Hacker"
 sudo systemctl restart ntp
 source ~/.bashrc
-date | tee kalisetupfinishdate.txt
+date | tee kalisetup.log
 updatedb
+# Buh Bye
 reboot
