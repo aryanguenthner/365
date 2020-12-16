9396fd334b7475b340d970b2353852885722b027
Aryan <aryanguenthner@users.noreply.github.com>
2020-12-16 09:14:36 -0800

    Add files via upload

--- metasploit-setup.sh
+++ metasploit-setup.sh
@@ -1,12 +1,11 @@
 # Metasploit Setup
 # Kali 2020.4
 
-sudo gem install rails
-
 cd /opt
 sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
 
 echo "Metasploit Ready Up"
+cd /home/kali/Desktop
 sudo systemctl start postgresql
 sudo msfdb init
 msfconsole -qr
