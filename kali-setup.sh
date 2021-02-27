eeaa9634bcae8098ce595b6410d92a35b02c76bf
Aryan <aryanguenthner@users.noreply.github.com>
2021-02-26 18:14:17 -0800

    Add files via upload

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ################################################
 # Kali Post Setup Automation Script with ivre.rocks
-# Tested on Kali 2020.4
+# Tested on Kali 2021.1
 # If you're reading this pat yourself on the back
 # sudo dos2unix *.sh
 # sudo chmod +x *.sh
 # Usage: sudo ./kali-setup.sh | tee setup.log
 # Learn more at https://github.com/aryanguenthner/
-# Last Updated 02/21/2021
+# Last Updated 02/26/2021
 ################################################
 echo
 cd /home/kali/Desktop
@@ -19,8 +19,12 @@ echo
 echo
 echo "Be Patient, Installing Kali Dependencies"
 sudo apt update
-sudo apt -y install at bloodhound ipcalc nload openjdk-13-jdk python3-dev python3-venv python3.8-venv pip python3-pip python3-bottle python3-cryptography python3-dbus python3-future python3-matplotlib python3-mysqldb python3-openssl python3-pil python3-psycopg2 python3-pymongo python3-sqlalchemy python3-tinydb python3-py2neo crackmapexec hostapd dnsmasq gedit cupp nautilus dsniff build-essential cifs-utils cmake curl ffmpeg gimp git graphviz imagemagick libapache2-mod-php php-xml libappindicator3-1 libindicator3-7 libmbim-utils nfs-common openssl tesseract-ocr vlc wkhtmltopdf xsltproc xutils-dev driftnet websploit apt-transport-https openresolv screenfetch baobab speedtest-cli sendmail libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev awscli sublist3r w3m jq libreoffice
+sudo apt -y install at bloodhound ipcalc nload python3-dev python3-venv pip python3-pip python3-bottle python3-cryptography python3-dbus python3-future python3-matplotlib python3-mysqldb python3-openssl python3-pil python3-psycopg2 python3-pymongo python3-sqlalchemy python3-tinydb python3-py2neo crackmapexec hostapd dnsmasq gedit cupp nautilus dsniff build-essential cifs-utils cmake curl ffmpeg gimp git graphviz imagemagick libapache2-mod-php php-xml libmbim-utils nfs-common openssl tesseract-ocr vlc wkhtmltopdf xsltproc xutils-dev driftnet websploit apt-transport-https openresolv screenfetch baobab speedtest-cli sendmail libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev awscli sublist3r w3m jq libreoffice
 echo
+#openjdk-13-jdk did not install
+#libindicator3-7 did not install
+#python3.8-venv did not install
+#libappindicator3-1 did not install
 pip3 install --upgrade pip
 echo
 echo "Hacker TV" #Works with Python 3.9
@@ -410,6 +414,7 @@ echo "Hacker Hacker"
 sudo systemctl restart ntp
 source ~/.zshrc
 echo
+apt --fix-broken install
 updatedb
 echo
 date > kali-setup-finish-date.txt
