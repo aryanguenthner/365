e5b2ef7a041cb757c7a9d1960ee1125c0c6e1fad
Aryan <aryanguenthner@users.noreply.github.com>
2021-11-05 14:28:28 -0700

    Add files via upload

---
+++ plex_media_player_installer.sh
@@ -0,0 +1,7 @@
+# Tested on Kali Linux 2021.3
+# Last updated 11/05/2021
+mkdir -p ~/AppImages
+cd ~/AppImages
+wget https://knapsu.eu/data/plex/Plex_Media_Player_20210521-ae73e074_x64.AppImage
+sudo chmod +x Plex_Media_Player_20210521-ae73e074_x64.AppImage
+./Plex_Media_Player_20210521-ae73e074_x64.AppImage
