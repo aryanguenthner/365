ea8b6285d4727faa6856b14d598ecb830192a094
Aryan <aryanguenthner@users.noreply.github.com>
2022-01-30 10:03:13 -0800

    Add files via upload

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #######################################################
-# Discover targets by doing a ping sweep
+# Discover targets by performing an ICMP ping sweep
 # Hosts that responded to ICMP are output to targets.txt 
-# Learn More @ https://github.com/aryanguenthner/
-# Tested on Kali 2021.3
-# Last updated 11/15/2021
+# Learn more @ https://github.com/aryanguenthner/
+# Tested on Kali 2021.4
+# Last updated 01/30/2022
 ######################################################
-
+#
 YELLOW='033m'
 BLUE='034m'
 SUBNET=`ip r |awk 'NR==2' |awk '{print $1}'`
@@ -15,16 +15,20 @@ KALI=`hostname -I`
 EXT=`curl ifconfig.me`
 FILE=$(date +%Y%m%d).nmap-pingsweep
 RANDOM=$$
-
+NMAP_FILE=`$(date +%Y%m%d)_nmapscan_$RANDOM`
+#
+mkdir -p /home/kali/Desktop/testing/nmapscans
+cd /home/kali/Desktop/testing/nmapscans && chmod -R 777 .
+#
 echo $KALI | awk '{print $1}' > KALI.txt
-
+#
 echo
 echo -e "\e[033mGetting Network Information\e[0m"
 echo
-echo -e "\e[033mExternal IP\e[0m"
+echo -e "\e[033mKali Public IP\e[0m"
 echo $EXT
 echo
-echo -e "\e[033mKali IP\e[0m"
+echo -e "\e[033mKali Internal IP\e[0m"
 echo $KALI | awk '{print $1}'
 echo
 echo -e "\e[033mThe Target Subnet\e[0m"
@@ -40,6 +44,10 @@ cat $FILE | grep "Up" | awk '{print $2}' 2>&1 | tee targets.txt
 echo
 echo -e "\e[033mTarget List Ouput File -> targets.txt\e[0m"
 echo
-echo -e "\e[033mUse nmap to enumerate more info on your targets:\e[0m"
+echo -e "\e[033mStarting enumeration on hosts in targets.txt:\e[0m"
 echo
-echo nmap --stats-every=1m -T4 -Pn -vv -sTV -p- --open --script http-screenshot,vuln --min-rate=5000 --min-hostgroup=256 --min-parallelism=256 --max-retries 0 -iL targets.txt -oA /home/kali/Desktop/testing/$(date +%Y%m%d)_nmapscan_$RANDOM
+nmap --stats-every=1m -T4 -Pn -vv -sCVT -p- --open --script http-screenshot,vuln --min-rate=5000 --min-hostgroup=256 --min-parallelism=256 --max-retries=0 -iL targets.txt -oA /home/kali/Desktop/testing/nmapscans/$NMAP_FILE --stylesheet nmap-bootstrap.xsl && xsltproc -o nmap-scan.html nmap-bootstrap.xsl $NMAP_FILE && ./txt-alert.sh
+
+: '
+./txt-alert.sh && nmap -T4 -sCVS -Pn -p- -vvvv --open --stats-every=1m --min-rate=5000 --min-hostgroup=256 --min-parallelism=256 --max-retries=0 --script-timeout=2m --script=ssl-cert,ssl-enum-ciphers,ssl-heartbleed,sip-enum-users,sip-brute,sip-methods,rtsp-screenshot,rtsp-url-brute,rpcinfo,vnc-screenshot,x11-access,x11-screenshot,nfs-showmount,nfs-ls,smb-vuln-ms08-067,smb-vuln-ms17-010,smb-ls,smb-enum-shares,http-robots.txt.nse,http-webdav-scan,http-screenshot,http-enum --script-args=http-enum.basepath=200,http-auth --script-args=http-auth.path=/login,http-form-brute,http-sql-injection,http-ntlm-info --script-args=http-ntlm-info.root=/root/,http-git,http-open-redirect,http-vuln-cve2017-5638 --script-args=path=/welcome.action,http-open-proxy,socks-open-proxy,smtp-open-relay,ftp-anon,ftp-bounce,ms-sql-config,ms-sql-info,ms-sql-empty-password,mysql-info,mysql-empty-password,vnc-brute,vnc-screenshot,vmware-version,http-shellshock,http-default-accounts,http-passwd --script-args=http-passwd.root=/test/,smb-vuln-ms17-010,rdp-vuln-ms12-020,vuln,grab_beacon_config,vmware-version,smtp-vuln-cve2020-28017-through-28026-21nails.nse -iL targets.txt -oA /home/kali/Desktop/testing/nmapscans/$(date +%Y%m%d)_nmapscan --stylesheet nmap-bootstrap.xsl && ./txt-alert.sh
+'
