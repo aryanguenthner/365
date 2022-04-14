605253a2a027c847da9933fbf6dfe4037d9a2340
Aryan <aryanguenthner@users.noreply.github.com>
2022-04-13 17:31:53 -0700

    Add files via upload

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,57 +1,78 @@
 #!/bin/bash
 #######################################################
-# Discover targets by performing an ICMP ping sweep
+# Discover targets by doing a ping sweep
 # Hosts that responded to ICMP are output to targets.txt 
-# Learn more @ https://github.com/aryanguenthner/
-# Tested on Kali 2021.4
-# Last updated 02/04/2022
+# Learn More @ https://github.com/aryanguenthner/
+# Tested on Kali 2022.1
+# Last updated 04/13/2022
 ######################################################
-#
+# Setting Variables
 YELLOW='033m'
 BLUE='034m'
 SUBNET=`ip r |awk 'NR==2' |awk '{print $1}'`
 TARGETS=ips.txt
 KALI=`hostname -I`
 EXT=`curl ifconfig.me`
-FILE=$(date +%Y%m%d).nmap-pingsweep
 RANDOM=$$
-NMAP_FILE=$(date +%Y%m%d)_nmapscan_$RANDOM
-PWD=pwd
+FILE0=$(date +%Y%m%d).nmap-pingsweep_$RANDOM
+FILE1=$(date +%Y%m%d).nmapscan_$RANDOM
 echo
-mkdir -p /home/kali/Desktop/testing/nmapscans
-cd /home/kali/Desktop/testing/nmapscans && chmod -R 777 .
+# Nmap bootstrap file checker
+# If file exists skip the download
+# if file is missing download it
+N='nmap-bootstrap.xsl'
+echo "Nmap Bootstrap File Checker"
+echo
+if [ -f $N ]
+then
+    echo "File Found: nmap-bootstrap.xsl"
+
+else
+
+    echo "Downloading Missing File"
+    wget https://raw.githubusercontent.com/aryanguenthner/nmap-bootstrap-xsl/stable/nmap-bootstrap.xsl
+
+fi
 echo
 echo $KALI | awk '{print $1}' > KALI.txt
 echo
+echo
 echo -e "\e[033mGetting Network Information\e[0m"
 echo
-echo -e "\e[033mKali Public IP\e[0m"
+echo -e "\e[033mExternal IP\e[0m"
 echo $EXT
 echo
-echo -e "\e[033mKali Internal IP\e[0m"
+echo -e "\e[033mKali IP\e[0m"
 echo $KALI | awk '{print $1}'
 echo
 echo -e "\e[033mThe Target Subnet\e[0m"
 echo $SUBNET
 echo
 echo -e "\e[033mGenerating a Target List\e[0m"
-nmap --stats-every=1m -sn -n $SUBNET --exclude $KALI -oG $(date +%Y%m%d).nmap-pingsweep
+nmap --stats-every=1m -sn -n $SUBNET --exclude $KALI -oG $FILE0
 echo
-echo -e "\033[33;5mThese Hosts Are Up\033[0m"
+echo -e "\e[033mPing Sweep Completed\e[0m"
 echo
-cat $FILE | grep "Up" | awk '{print $2}' 2>&1 | tee targets.txt
+#echo -e "\e[033mThese Hosts Are Up\e[0m"
+echo  -e "\033[33;5mThese Hosts Are Up\033[0m"
+echo
+cat $FILE0 | grep "Up" | awk '{print $2}' 2>&1 | tee targets.txt
 echo
 echo -e "\e[033mTarget List Ouput File -> targets.txt\e[0m"
 echo
-echo -e "\e[033mStarting enumeration on hosts in targets.txt:\e[0m"
+echo -e "\e[033m***Using nmap to enumerate more info on your targets***\e[0m"
 echo
-# Quickie
-#nmap --stats-every=1m -T4 -Pn -vvvv -g 53 -A -sC -p0- --open --reason T:1-65535 U:53,88,161 --script=http-screenshot --max-retries=0 -iL targets.txt -oA /home/kali/Desktop/testing/nmapscans/$NMAP_FILE --stylesheet nmap-bootstrap.xsl && xsltproc -o $NMAP_FILE.html nmap-bootstrap.xsl $NMAP_FILE.xml && ./txt-alert.sh
-echo
-# TODO: %D
-# Thorough
-nmap --stats-every=1m -T4 -Pn -vvvv -g 0 -sVSC -A -p0- --open --reason  T:1-65535 U:53,88,161 --min-rate=1024--min-hostgroup=256 --min-parallelism=256 --script=ssl-cert,ssl-enum-ciphers,ssl-heartbleed,sip-enum-users,sip-brute,sip-methods,rtsp-screenshot,rtsp-url-brute,rpcinfo,vnc-screenshot,x11-access,x11-screenshot,nfs-showmount,nfs-ls,smb-vuln-ms08-067,smb-vuln-ms17-010,smb-ls,smb-enum-shares,http-robots.txt.nse,http-webdav-scan,http-screenshot,http-enum --script-args=http-enum.basepath=200,http-auth --script-args=http-auth.path=/login,http-form-brute,http-sql-injection,http-ntlm-info --script-args=http-ntlm-info.root=/root/,http-git,http-open-redirect,http-vuln-cve2017-5638 --script-args=path=/welcome.action,http-open-proxy,socks-open-proxy,smtp-open-relay,ftp-anon,ftp-bounce,ms-sql-config,ms-sql-info,ms-sql-empty-password,mysql-info,mysql-empty-password,vnc-brute,vnc-screenshot,vmware-version,http-shellshock,http-default-accounts,http-passwd --script-args=http-passwd.root=/test/,smb-vuln-ms17-010,rdp-vuln-ms12-020,vuln,grab_beacon_config,vmware-version,smtp-vuln-cve2020-28017-through-28026-21nails.nse -iL targets.txt -oA /home/kali/Desktop/testing/nmapscans/$NMAP_FILE --stylesheet nmap-bootstrap.xsl && xsltproc -o $NMAP_FILE.html nmap-bootstrap.xsl $NMAP_FILE.xml && ./txt-alert.sh
-echo
-echo $PWD/$NMAP_FILE.html
+echo "Hack The Planet Happening Now"
 echo
 echo
+nmap --stats-every=1m -T4 -Pn -vv -sCTV -p- --open --script http-screenshot,http-passwd --script-args http-passwd.root=/test/ --min-rate=1000 --min-hostgroup=128 --min-parallelism=128 --max-retries 0 -oA /home/kali/Desktop/testing/$FILE1 --stylesheet nmap-bootstrap.xsl $SUBNET
+echo
+xsltproc -o $FILE1.html nmap-bootstrap.xsl $FILE1.xml
+echo
+# TODO su kali
+# firefox $FILE1.html
+# Pay me later
+
+: 'ssl-cert,ssl-enum-ciphers,ssl-heartbleed,sip-enum-users,sip-brute,sip-methods,rtsp-screenshot,rtsp-url-brute,rpcinfo,vnc-screenshot,x11-access,x11-screenshot,nfs-showmount,nfs-ls,smb-vuln-ms08-067,smb-vuln-ms17-010,smb-ls,smb-enum-shares,http-robots.txt.nse,http-webdav-scan,http-screenshot,http-enum --script-args=http-enum.basepath=200,http-auth --script-args=http-auth.path=/login,http-form-brute,http-sql-injection,http-ntlm-info --script-args=http-ntlm-info.root=/root/,http-git,http-open-redirect,http-vuln-cve2017-5638 --script-args=path=/welcome.action,http-open-proxy,socks-open-proxy,smtp-open-relay,ftp-anon,ftp-bounce,ms-sql-config,ms-sql-info,ms-sql-empty-password,mysql-info,mysql-empty-password,vnc-brute,vnc-screenshot,vmware-version,http-shellshock,http-default-accounts,http-passwd --script-args=http-passwd.root=/test/,smb-vuln-ms17-010,rdp-vuln-ms12-020,vuln,grab_beacon_config,vmware-version,smtp-vuln-cve2020-28017-through-28026-21nails.nse
+
+'
