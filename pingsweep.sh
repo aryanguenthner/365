30993252e62ab914134bc0fb4040e086427073ce
Aryan <aryanguenthner@users.noreply.github.com>
2022-05-29 14:29:24 -0700

    Add files via upload

--- pingsweep.sh
+++ pingsweep.sh
@@ -3,8 +3,8 @@
 # Discover targets by doing a ping sweep
 # Hosts that responded to ICMP are output to targets.txt 
 # Learn More @ https://github.com/aryanguenthner/
-# Tested on Kali 2022.1
-# Last updated 04/13/2022
+# Tested on Kali 2022.2
+# Last updated 05/29/2022
 ######################################################
 # Setting Variables
 YELLOW='033m'
@@ -34,6 +34,33 @@ else
 
 fi
 echo
+# PhantomJS Checker
+# Used for nmap screenshots
+echo "PhantomJS Checker"
+echo
+P=`phantomjs -v`
+
+if [ $P=1.9.8 ]
+then
+
+    echo "Found PhantomJS"
+echo
+phantomjs -v
+else
+
+    echo "Downloading PhantomJS"
+cd /tmp
+echo
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+echo
+echo "Extracting and Installing PhantomJS 1.9.8"
+tar xvf phantomjs-1.9.8-linux-x86_64.tar.bz2
+mv phantomjs-1.9.8-linux-x86_64 phantomjs
+mv phantomjs /opt
+ln -s /opt/phantomjs/bin/phantomjs /usr/local/bin/phantomjs
+phantomjs -v
+
+fi
 echo $KALI | awk '{print $1}' > KALI.txt
 echo
 echo
@@ -65,7 +92,7 @@ echo
 echo "Hack The Planet Happening Now"
 echo
 echo
-nmap --stats-every=1m -T4 -Pn -vv -sCTV -p- --open --script http-screenshot,http-passwd --script-args http-passwd.root=/test/ --min-rate=1000 --min-hostgroup=128 --min-parallelism=128 --max-retries 0 -oA /home/kali/Desktop/testing/$FILE1 --stylesheet nmap-bootstrap.xsl $SUBNET
+nmap --stats-every=1m -T4 -Pn -vvvv -sCV -p- --open --script http-screenshot --min-rate=256 --min-hostgroup=256 --min-parallelism=256 --max-retries 0 -oA /home/kali/Desktop/testing/$FILE1 --stylesheet nmap-bootstrap.xsl $SUBNET
 echo
 xsltproc -o $FILE1.html nmap-bootstrap.xsl $FILE1.xml
 echo
@@ -73,6 +100,6 @@ echo
 # firefox $FILE1.html
 # Pay me later
 
-: 'ssl-cert,ssl-enum-ciphers,ssl-heartbleed,sip-enum-users,sip-brute,sip-methods,rtsp-screenshot,rtsp-url-brute,rpcinfo,vnc-screenshot,x11-access,x11-screenshot,nfs-showmount,nfs-ls,smb-vuln-ms08-067,smb-vuln-ms17-010,smb-ls,smb-enum-shares,http-robots.txt.nse,http-webdav-scan,http-screenshot,http-enum --script-args=http-enum.basepath=200,http-auth --script-args=http-auth.path=/login,http-form-brute,http-sql-injection,http-ntlm-info --script-args=http-ntlm-info.root=/root/,http-git,http-open-redirect,http-vuln-cve2017-5638 --script-args=path=/welcome.action,http-open-proxy,socks-open-proxy,smtp-open-relay,ftp-anon,ftp-bounce,ms-sql-config,ms-sql-info,ms-sql-empty-password,mysql-info,mysql-empty-password,vnc-brute,vnc-screenshot,vmware-version,http-shellshock,http-default-accounts,http-passwd --script-args=http-passwd.root=/test/,smb-vuln-ms17-010,rdp-vuln-ms12-020,vuln,grab_beacon_config,vmware-version,smtp-vuln-cve2020-28017-through-28026-21nails.nse
+: 'Great Enumeration Scripts -> ssl-cert,ssl-enum-ciphers,ssl-heartbleed,sip-enum-users,sip-brute,sip-methods,rtsp-screenshot,rtsp-url-brute,rpcinfo,vnc-screenshot,x11-access,x11-screenshot,nfs-showmount,nfs-ls,smb-vuln-ms08-067,smb-vuln-ms17-010,smb-ls,smb-enum-shares,http-robots.txt.nse,http-webdav-scan,http-screenshot,http-enum --script-args=http-enum.basepath=200,http-auth --script-args=http-auth.path=/login,http-form-brute,http-sql-injection,http-ntlm-info --script-args=http-ntlm-info.root=/root/,http-git,http-open-redirect,http-vuln-cve2017-5638 --script-args=path=/welcome.action,http-open-proxy,socks-open-proxy,smtp-open-relay,ftp-anon,ftp-bounce,ms-sql-config,ms-sql-info,ms-sql-empty-password,mysql-info,mysql-empty-password,vnc-brute,vnc-screenshot,vmware-version,http-shellshock,http-default-accounts,http-passwd --script-args=http-passwd.root=/test/,smb-vuln-ms17-010,rdp-vuln-ms12-020,vuln,grab_beacon_config,vmware-version,smtp-vuln-cve2020-28017-through-28026-21nails.nse
 
 '
