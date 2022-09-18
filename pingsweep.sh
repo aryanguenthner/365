ce57120cba038532bfccbb4e21b65ba0ebfdbdfe
Aryan <aryanguenthner@users.noreply.github.com>
2022-09-17 19:29:17 -0700

    Add files via upload

--- pingsweep.sh
+++ pingsweep.sh
@@ -103,14 +103,14 @@ echo  -e "\033[33;5mThese $TARGETCOUNT Hosts Are Up\033[0m"
 echo
 cat $FILE0 | grep "Up" | awk '{print $2}' 2>&1 | tee targets.txt
 echo
-echo -e "\e[033mTarget List Ouput File -> targets.txt\e[0m"
+echo -e "\e[033mTarget List File -> targets.txt\e[0m"
 echo
 echo -e "\e[033m***Using nmap to enumerate more info on your targets***\e[0m"
 echo
-echo -e "Hack The Planet"
+echo -e "\e[034mHack The Planet\e[0m"
 echo
 # Nmap Scan Syntax
-nmap -iL $TARGETS --stats-every=1m -T4 -Pn -sCV -p- --open --reason -vvvv --exclude $KALI --script=http-screenshot --min-rate=256 --max-retries=0 -oA "/home/kali/Desktop/testing/nmapscans/$FILE1" --stylesheet $BOOTSTRAP
+nmap -iL $TARGETS --stats-every=1m -T4 -Pn -sCV -p- --open -vvvv --exclude $KALI --script=http-screenshot --min-rate=256 --max-retries=0 -oA "/home/kali/Desktop/testing/nmapscans/$FILE1" --stylesheet $BOOTSTRAP
 echo
 xsltproc -o $FILE1.html $BOOTSTRAP $FILE1.xml
 echo
@@ -119,7 +119,7 @@ echo
 echo "Nmap scan completed"
 echo $(pwd)/$FILE1.html
 echo
-# firefox $FILE1.html
+
 # Pay me later
 
 : 'Great Enumeration Scripts -> ssl-cert,ssl-enum-ciphers,ssl-heartbleed,sip-enum-users,sip-brute,sip-methods,rtsp-screenshot,rtsp-url-brute,rpcinfo,vnc-screenshot,x11-access,x11-screenshot,nfs-showmount,nfs-ls,smb-vuln-ms08-067,smb-vuln-ms17-010,smb-ls,smb-enum-shares,http-robots.txt.nse,http-webdav-scan,http-screenshot,http-enum --script-args=http-enum.basepath=200,http-auth --script-args=http-auth.path=/login,http-form-brute,http-sql-injection,http-ntlm-info --script-args=http-ntlm-info.root=/root/,http-git,http-open-redirect,http-vuln-cve2017-5638 --script-args=path=/welcome.action,http-open-proxy,socks-open-proxy,smtp-open-relay,ftp-anon,ftp-bounce,ms-sql-config,ms-sql-info,ms-sql-empty-password,mysql-info,mysql-empty-password,vnc-brute,vnc-screenshot,vmware-version,http-shellshock,http-default-accounts,http-passwd --script-args=http-passwd.root=/test/,smb-vuln-ms17-010,rdp-vuln-ms12-020,vuln,grab_beacon_config,vmware-version,smtp-vuln-cve2020-28017-through-28026-21nails.nse
