6d7cd1d9882ef5ce53a58a078f70d089d5b4ea9b
Aryan <aryanguenthner@users.noreply.github.com>
2022-11-11 19:42:11 -0800

    Add files via upload

--- uRLwitness-DEV.sh
+++ uRLwitness-DEV.sh
@@ -4,10 +4,9 @@
 # Reconnaissance Tool used to scrape URLs from a website
 # Open http://localhost:7171 to view the Gowitness report
 # Tested on Kali 2022.4
-# If you're reading this pat yourself on the back
 # Learn more at https://github.com/aryanguenthner/
 # credit: https://github.com/sensepost/gowitness/releases
-# Last Updated 11/5/2022
+# Last Updated 11/11/2022
 ################################################
 echo "
 ██╗░░░██╗██████╗░██╗░░░░░░██╗░░░░░░░██╗██╗████████╗███╗░░██╗███████╗░██████╗░██████╗
@@ -16,9 +15,9 @@ echo "
 ██║░░░██║██╔══██╗██║░░░░░░░████╔═████║░██║░░░██║░░░██║╚████║██╔══╝░░░╚═══██╗░╚═══██╗
 ╚██████╔╝██║░░██║███████╗░░╚██╔╝░╚██╔╝░██║░░░██║░░░██║░╚███║███████╗██████╔╝██████╔╝
 ░╚═════╝░╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚══╝╚══════╝╚═════╝░╚═════╝░"
-
+echo "uRLwitness v1.0"
 echo
-# wget -4 -qO- https://www.defense.gov/Resources/Military-Departments/DOD-Websites/category/ | grep -Eo (http|https)://[a-zA-Z0-9./?=_-]* | sort -u | tee url-results.txt
+# wget --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9' https://www.defense.gov/Resources/Military-Departments/DOD-Websites/category/ -qO- --no-check-certificate --progress=bar --show-progress --ignore-length --ignore-case -H --max-redirect=1024 --follow-tags=* --random-wait --trust-server-names --connect-timeout=4 | grep -Eo (http|https)://[a-zA-Z0-9./?=_-]* | sort -u | tee url-results.txt
 # Remove http,https,www
 # cat url-results.txt | sed -e 's/^http:\/\///g' -e 's/^https:\/\///g' -e 's/^www.//g' | tee dod-domains.txt
 
@@ -31,6 +30,7 @@ LS=`ls -l`
 PWD=`pwd`
 SORT="sort -u"
 RANDOM=$$
+UA='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
 echo
 
 # Stay Organized
@@ -50,7 +50,7 @@ echo
 
 # Networking
 echo -e "\e[034mPublic IP Address\e[0m"
-curl ifconfig.me
+dig +short @resolver1.opendns.com myip.opendns.com
 echo
 
 echo -e "\e[034mKali IP Address\e[0m"
@@ -83,23 +83,25 @@ then
 
 else
   echo -e "\e[034mDownloading Missing $GWIT File\e[0m"
+
+cd /home/kali/Desktop/testing/urlwitness
 wget -O $PWD/gowitness-2.4.2-linux-amd64 https://github.com/aryanguenthner/gowitness/releases/download/gowitness/gowitness-2.4.2-linux-amd64
 
 fi
 echo
 
-# Start urlwitness
+# Start uRLwitness
 cd /home/kali/Desktop/testing/urlwitness
-echo -en "\e[034mEnter the site to scrape URLs:\e[0m"
+echo -en "\e[034mEnter the site to scrape URLs: \e[0m"
 read URL
 echo
 
 # wget magic
-wget -4 -qO- --referer=$URL $URL --random-wait --trust-server-names --show-progress --no-check-certificate ‐‐user-agent=Mozilla --connect-timeout=4 | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | $SORT > urls.txt
+wget -d --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)" -4 --https-only --secure-protocol=PFS -e --robots=off -H --compression=auto --adjust-extension --trust-server-names --max-redirect=24 --no-cache --progress=bar --show-progress --random-wait --connect-timeout=4 --header "content-type: text/html; charset=utf-8" -qO- $URL | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | $SORT > urls.txt
 echo
 
 # Strippers
-grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\|ico\|jpeg\|woff\|woff2\|svg\|wav\|mp4\|mp3\|dtd\|eot\|ttf\|webp\)$' urls.txt | tee url-results.txt
+grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\|ico\|jpeg\|woff\|woff2\|svg\|wav\|mp4\|mp3\|dtd\|eot\|ttf\|webp\|map\)$' urls.txt | tee url-results.txt
 sed -i '/s.yimg/d' url-results.txt
 
 # Get domains
@@ -136,12 +138,14 @@ echo
 # Get Screenshots
 echo "Getting Screenshots"
 echo
+sleep 2
 chmod -R 777 /home/kali/
 ./gowitness-2.4.2-linux-amd64 file -f url-results.txt
 echo
 echo  -e "\033[33;5mReport Finished http://localhost:7171\033[0m"
 echo
-./gowitness-2.4.2-linux-amd64 server report
+./gowitness-2.4.2-linux-amd64 server report;
+sudo su -c "firefox http://localhost:7171" kali
 echo
 
 # May the force be with you!
