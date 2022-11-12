ea71d2cc5579fb8379698a43f5e837f98fe1b85e
Aryan <aryanguenthner@users.noreply.github.com>
2022-11-12 11:24:06 -0800

    Add files via upload

--- start-tcpdump.sh
+++ start-tcpdump.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 ################################################
-# Tested on Kali 2019.3
-# This Is The First Thing I Do On Any Penetration Test Engagement 
-# Do This: dos2unix *.sh then chmod +x *.sh
+# Tested on Kali 2022.4
+# Step 1 for any security testing
 # Usage: ./start-tcpdump.sh
 # Learn more at https://github.com/aryanguenthner
-# Last Updated 2020-03-26
+# Last Updated 2022-11-12
 ################################################
+# Working Directory
+echo -e "\e[034mCurrent Directory\e[0m"
+echo $PWD
+echo
+
+# Todays Date
+echo -e "\e[034mToday is\e[0m"
+date
+echo
+
+# Networking
+echo -e "\e[034mPublic IP Address\e[0m"
+curl ifconfig.me
+echo
+
+echo -e "\e[034mKali IP Address\e[0m"
+echo $KALI
+echo
 echo
 echo "I recommend putting this script in screen"
 echo
