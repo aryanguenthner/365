e599e30a16aa0bee5a3e7aa235c646f9eaecd0af
Aryan <aryanguenthner@users.noreply.github.com>
2023-09-01 20:43:33 -0700

    Add files via upload

--- hacker-tv-installer.sh
+++ hacker-tv-installer.sh
@@ -8,10 +8,25 @@ echo
 echo "Power off VM, Open VirtualBox, Settings > Display > Extended Features Enable 3D Acceleration"
 sleep 1
 echo
-
 sudo apt-get update
 echo "Hacker TV"
 echo
+
+# Hypnotix checker
+TV=/bin/hypnotix
+if [ -f "$TV" ]
+then
+    echo "Found hypnotix"
+
+else
+
+# Download Hypnotix
+wget --no-check-certificate https://github.com/linuxmint/hypnotix/releases/download/master.mint21/packages.tar.gz > /dev/null 2>&1
+tar -xf packages.tar.gz > /dev/null 2>&1
+rm packages.tar.gz
+
+chmod -R 777 /opt/packages && cd /opt/packages
+dpkg -i *.deb
 
 echo "Installing Dependencies"
 echo
@@ -22,69 +37,56 @@ wget --no-check-certificate http://ftp.us.debian.org/debian/pool/main/i/imdbpy/p
 dpkg -i python3-imdbpy_6.6-1_all.deb > /dev/null 2>&1
 rm python3-imdbpy_6.6-1_all.deb
 echo
-
-# Download Hypnotix
-wget --no-check-certificate https://github.com/linuxmint/hypnotix/releases/download/master.mint21/packages.tar.gz > /dev/null 2>&1
-tar -xf packages.tar.gz > /dev/null 2>&1
-rm packages.tar.gz
-
-# Get icon
-ICON=/home/kali/Pictures/tv.png
-if [ -f "$ICON" ]
-then
-
-    echo "Found TV icon"
-
-else
-
-    echo "Downloading TV icon"
-wget -O /home/kali/Pictures/tv.png https://raw.githubusercontent.com/aryanguenthner/365/e7a68a70eda392ba6b4b1cbb99e405e3ad677c4d/tv.png > /dev/null 2>&1
-
-fi
-
-chmod -R 777 /opt/packages && cd /opt/packages
-dpkg -i *.deb
-
 # Insurance
 sudo apt-get --fix-broken install -y
 updatedb
+
+fi
 echo
-echo "Pro Tip"
+
+# hacker.tv checker
+H=/home/kali/Desktop/hacker.tv
+if [ -f "$H" ]
+then
+    echo "Found hacker.tv"
+
+else
+
+# Create hacker.tv launcher
+echo "Creating hacker.tv launcher"
+
+cat <<EOF > /home/kali/Desktop/hacker.tv
+Type=Application
+Name=hacker.tv
+Terminal=false
+Exec=sudo su -c "hypnotix" kali > /dev/null 2>&1
+Comment=Watch TV
+Path=/home/kali/Desktop/hacker.tv
+StartupNotify=false
+MimeType=text/plain
+EOF
+
+chmod a+x /home/kali/Desktop/hacker.tv
+chmod -R 777 /home/kali/Desktop/hacker.tv    
+    
+fi
 echo
+
+echo "Confirm Enable 3D Acceleration in VM Settings"
+echo "Power off VM, Open VirtualBox, Settings > Display > Extended Features Enable 3D Acceleration"
+echo "If you already did this step, Ignore this message"
+sleep 1
+echo
+echo "Tip"
 echo "Add this Provider -> https://iptv-org.github.io/iptv/index.m3u"
 echo
 echo "IPTV Provider with full version: https://iptv-org.github.io/iptv/index.nsfw.m3u"
 sleep 1
 echo
-
-# Create hacker.tv launcher
-echo "Creating hacker.tv launcher"
-cat <<EOF > /home/kali/Desktop/tv.Desktop
-[Desktop Entry]
-Type=Application
-Name=hacker.tv
-#Terminal=false
-Exec=sudo su -c "hypnotix" kali > /dev/null 2>&1
-Icon=/home/kali/Pictures/tv.png
-Comment=Watch TV
-Path=/home/kali/Desktop/tv.Desktop
-StartupNotify=false
-EOF
-
-chmod a+x /home/kali/Desktop/tv.Desktop
-chmod -R 777 /home/kali/Desktop/tv.Desktop
-echo "Confirm Enable 3D Acceleration in VM Settings"
-echo
-echo "Power off VM, Open VirtualBox, Settings > Display > Extended Features Enable 3D Acceleration"
-echo
-echo "If you already did this step, Ignore this message"
+echo "Watch TV enter: ./hacker.tv"
 sleep 1
 echo
-echo "Watch TV enter: ./tv.Desktop"
-sleep 1
-echo
-
-./tv.Desktop > /dev/null 2>&1
+bash hacker.tv > /dev/null 2>&1
 echo
 
 cd /home/kali/Desktop
