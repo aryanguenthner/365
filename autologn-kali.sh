280c9a073746beecae4ca11a2a9e60a3c27fe2e4
Aryan <aryanguenthner@users.noreply.github.com>
2024-07-13 21:39:56 -0700

    Add files via upload

--- autologn-kali.sh
+++ autologn-kali.sh
@@ -1,8 +1,8 @@
 #sudo nano /etc/lightdm/lightdm.conf
 echo
 echo
-sed -i '126s/#autologin-user=/autologin-user=kali/g' /etc/lightdm/lightdm.conf
-sed -i '127s/#autologin-user-timeout=0/autologin-user-timeout=0/g' /etc/lightdm/lightdm.conf
+sed -i '120s/#autologin-user=/autologin-user=kali/g' /etc/lightdm/lightdm.conf
+sed -i '121s/#autologin-user-timeout=0/autologin-user-timeout=0/g' /etc/lightdm/lightdm.conf
 echo
 
 sudo service lightdm restart
