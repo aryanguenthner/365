94b8ecbd393469e358446f934b1970af7496f4d6
Aryan <aryanguenthner@users.noreply.github.com>
2025-02-01 10:11:10 -0800

    Add files via upload

--- autologn-kali.sh
+++ autologn-kali.sh
@@ -7,5 +7,11 @@ sed -i 's/#autologin-user-timeout=0/autologin-user-timeout=0/' /etc/lightdm/ligh
 echo
 
 sudo service lightdm restart
+
+# Fix Unlock login keyring
+# cd /home/kali/Downloads
+# cp /home/kali/.local/share/keyrings/login.keyring login.keyring.backup
+# rm /home/kali/.local/share/keyrings/login.keyring                    
+
 reboot
 echo
