77990c2fa505df598bc59d9e58a832b5b31a7022
Aryan <aryanguenthner@users.noreply.github.com>
2025-02-16 20:54:32 -0800

    Add files via upload

--- darksheets-v1.2.sh
+++ darksheets-v1.2.sh
@@ -3,7 +3,7 @@
 # Made for OSINT CTI cyber security research on the Dark Deep Web
 # Intended to be used on Kali Linux
 # Updated for compatibility and better Tor handling
-# Hacked on 02/15/2025, pay me later
+# Hacked on 02/16/2025, pay me later
 # Great ideas
 # install_addon "https://addons.mozilla.org/firefox/downloads/file/4141345/noscript-11.4.26.xpi" "noscript"
 # install_addon "https://addons.mozilla.org/firefox/downloads/file/4125998/adblock_plus-3.17.1.xpi" "adblock_plus"
