a4ce21a106f889083f62920e7afbbefe79211836
Aryan <aryanguenthner@users.noreply.github.com>
2025-02-23 21:40:41 -0800

    Add files via upload

--- public-ip-xfce-panel.sh
+++ public-ip-xfce-panel.sh
@@ -1,9 +1,7 @@
 # Public IP xfce panel generic monitor
-EXT=$(curl -s https://api64.ipify.org || curl -s https://ifconfig.me || curl -s https://checkip.amazonaws.com)
 LOCATION=$(curl -s ipinfo.io/json)
 COUNTRY=$(echo "$LOCATION" | jq -r '.country')
 REGION=$(echo "$LOCATION" | jq -r '.region')
 CITY=$(echo "$LOCATION" | jq -r '.city')
-echo "$EXT"
 echo "$CITY"
-echo "$COUNTRY" "$REGION"
+echo "$REGION" "$COUNTRY"
--- wifi-ssid-xfce-panel.sh
+++ wifi-ssid-xfce-panel.sh
@@ -1,3 +1,7 @@
 # WiFi SSID xfce panel generic monitor
 SSID=$(iw dev wlan0 info | grep ssid | awk '{print $2}')
+IP=$(hostname -I | awk '{print $1}')
+EXT=$(curl -s https://api64.ipify.org || curl -s https://ifconfig.me || curl -s https://checkip.amazonaws.com)
 echo $SSID
+echo $IP
+echo $EXT
